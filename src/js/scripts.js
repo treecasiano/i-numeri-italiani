be52b84d84ad39d7746976f473b48dd498f334bb
Tree Casiano <tree.pdx@gmail.com>
2015-11-22 21:31:21 -0800

    Clean up indentation.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,25 +1,27 @@
 $( document ).ready(function() {
 
-    var numToTranslate;
+  var numToTranslate;
 
-    function generateNumber () {
-        return Math.floor(Math.random() * (999)) + 1;
-    }
+  function generateNumber () {
+    return Math.floor(Math.random() * (9999)) + 1;
+  }
 
-    $("#generateNum").on("click", function() {
-        $("#showNum").text(generateNumber());
-        numToTranslate = $("#showNum").text();
-        console.log("number to be translated = " + numToTranslate);
-    });
+  $("#generateNum").on("click", function() {
+    $("#showNum").text(generateNumber());
+    numToTranslate = $("#showNum").text();
+    console.log("number to be translated = " + numToTranslate);
+  });
 
-    $("#submitTranslation").on ("click", function() {
-        var request = {
-            text: numToTranslate
-        };
+  $("#submitTranslation").on ("click", function() {
+    var request = {
+    text: numToTranslate
+  };
 
-        $.post("translate", request, function(response) {
-            $("#showTranslation").text(response.translation);
-    });
-        console.log(request);
-    });
+  $.post("translate", request, function(response) {
+    $("#showTranslation").text(response.translation);
+  });
+
+  console.log(request);
+
+  });
 });
