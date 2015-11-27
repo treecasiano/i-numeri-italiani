cb27d0a571f09ff5bd3fba59b750e1b5d4becda5
Tree Casiano <tree.pdx@gmail.com>
2015-11-27 08:59:41 -0800

    Remove gulp-nodemon.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,43 +3,33 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-var nodemon = require('gulp-nodemon');
 var mocha = require('gulp-mocha');
 
 gulp.task('styles', function() {
-    gulp.src('src/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./app/css/'));
+  gulp.src('src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css/'));
 });
 
 gulp.task('scripts', function() {
-    gulp.src('src/js/**/*.js')
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('./app/js/'))
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./app/js/'));
+  gulp.src('src/js/**/*.js')
+  .pipe(concat('scripts.js'))
+  .pipe(gulp.dest('./app/js/'))
+  .pipe(uglify())
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest('./app/js/'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/js/**/*.js', ['scripts']);
-    gulp.watch('src/sass/*.scss', ['styles']);
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch('src/sass/*.scss', ['styles']);
 });
 
 gulp.task('tests', function() {
-    gulp.src('test/test.js', {read: false})
-        .pipe(mocha({reporter: 'spec'}));
+  gulp.src('test/test.js', {read: false})
+    .pipe(mocha({reporter: 'spec'}));
 });
-// gulp.task('start', function () {
-//   nodemon ({
-//     script: 'app.js',
-//     ext: 'html js css',
-//     watch: 'app.js',
-//     tasks: ['scripts', 'styles'],
-//     ignore: ['node_modules/**'],
-//     });
-// });
 
 gulp.task('default', ['styles', 'scripts', 'tests', 'watch']);
