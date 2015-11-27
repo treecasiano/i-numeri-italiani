d88df5cbd9dba23a11c37a6b77e1a0e36acbee60
Tree Casiano <tree.pdx@gmail.com>
2015-11-27 12:15:08 -0800

    Clean up code style.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,35 +1,35 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var mocha = require('gulp-mocha');
+var gulp = require( "gulp" );
+var sass = require( "gulp-sass" );
+var concat = require( "gulp-concat" );
+var rename = require( "gulp-rename" );
+var uglify = require( "gulp-uglify" );
+var mocha = require( "gulp-mocha" );
 
-gulp.task('styles', function() {
-  gulp.src('src/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./app/css/'));
-});
+gulp.task( "styles", function() {
+  gulp.src( "src/sass/**/*.scss" )
+    .pipe( sass().on( "error", sass.logError ) )
+    .pipe( gulp.dest( "./app/css/" ) );
+} );
 
-gulp.task('scripts', function() {
-  gulp.src('src/js/**/*.js')
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('./app/js/'))
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./app/js/'));
-});
+gulp.task( "scripts", function() {
+  gulp.src( "src/js/**/*.js" )
+    .pipe( concat( "scripts.js" ) )
+    .pipe( gulp.dest( "./app/js/" ) )
+    .pipe( uglify() )
+    .pipe( rename( {
+      suffix: ".min"
+    } ) )
+    .pipe( gulp.dest( "./app/js/" ) );
+} );
 
-gulp.task('watch', function() {
-  gulp.watch('src/js/**/*.js', ['scripts']);
-  gulp.watch('src/sass/*.scss', ['styles']);
-});
+gulp.task( "watch", function() {
+  gulp.watch( "src/js/**/*.js", [ "scripts" ] );
+  gulp.watch( "src/sass/*.scss", [ "styles" ] );
+} );
 
-gulp.task('tests', function() {
-  gulp.src('test/test.js', {read: false})
-    .pipe(mocha({reporter: 'spec'}));
-});
+gulp.task( "tests", function() {
+  gulp.src( "test/test.js", { read: false } )
+    .pipe( mocha( { reporter: "spec" } ) );
+} );
 
-gulp.task('default', ['styles', 'scripts', 'tests', 'watch']);
+gulp.task( "default", [ "styles", "scripts", "tests", "watch" ] );
