4741fdde6a64f0a0b300e44927cc4cb8ba438382
Tree Casiano <tree.pdx@gmail.com>
2019-12-15 14:32:56 -0800

    Clean up linting errors

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,8 +1,8 @@
 import { shallowMount } from "@vue/test-utils";
 import NumberToTextTranslator from "@/components/NumberToTextTranslator.vue";
 
-skip.describe("NumberToTextTranslator.vue", () => {
-  it("renders props.msg when passed", () => {
+describe("NumberToTextTranslator.vue", () => {
+  it.skip("renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(NumberToTextTranslator, {
       propsData: { msg },
