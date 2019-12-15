7c453895c75fb1017da00b9ddb7113a17ef791bd
Tree Casiano <tree.pdx@gmail.com>
2019-12-15 13:38:23 -0800

    Add tabs

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-toolbar-title class="accent--text title">
-        I Numeri Italiani
-      </v-toolbar-title>
+      <v-toolbar-title class="accent--text title">I Numeri Italiani</v-toolbar-title>
+      <template v-slot:extension>
+        <v-tabs v-model="tabs" centered slider-color="accent" background-color="transparent">
+          <v-tab>Learn to Count in Italian</v-tab>
+          <v-tab>Translate Numbers</v-tab>
+          <v-tab>Practice Quiz</v-tab>
+        </v-tabs>
+      </template>
     </v-app-bar>
 
     <v-content>
-      <NumberToTextTranslator />
+      <v-tabs-items v-model="tabs" color="transparent">
+        <v-tab-item></v-tab-item>
+        <v-tab-item>
+          <NumberToTextTranslator />
+        </v-tab-item>
+        <v-tab-item></v-tab-item>
+      </v-tabs-items>
     </v-content>
   </v-app>
 </template>
@@ -23,7 +34,7 @@ export default {
   },
 
   data: () => ({
-    //
+    tabs: null,
   }),
 };
 </script>
